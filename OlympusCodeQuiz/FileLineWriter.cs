f8a2b2730c1fb439905aba7759211538fc31f47e
ray_brierly <george.renato@gmail.com>
2023-11-09 11:11:19 -0500

    Initial commit

---
+++ OlympusCodeQuiz/FileLineWriter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlympusCodeQuiz
+{
+    internal class FileLineWriter
+    {
+        private string _directory;
+        private string _fileName;
+
+        private const string DATE_FORMAT = "hh:mm:ss.fff";
+
+        public FileLineWriter(string directory,
+            string fileName)
+        {
+            _directory = directory;
+            _fileName = fileName;
+        }
+
+        public void WriteLine(int threadId)
+        {
+            var filePath = GetFilePath();
+            var lines = File.ReadLines(filePath);
+            var counter = 0;
+            if (lines.Any())
+            {
+                var lastLine = lines.Last();
+                counter = GetCounter(lastLine);
+            }
+           
+            using (StreamWriter stream = File.AppendText(filePath))
+            {
+                counter++;
+                var timestamp = DateTime.Now.ToString(DATE_FORMAT);
+                var line = $"{counter},{threadId},{timestamp}";
+                stream.WriteLine(line);
+            }
+
+        }
+
+        public void InitFile()
+        {
+            Directory.CreateDirectory(_directory);
+
+            var filePath = GetFilePath();
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+
+            // add first line
+            using(StreamWriter stream = File.AppendText(filePath))
+            {
+                var timestamp = DateTime.Now.ToString(DATE_FORMAT);
+                stream.WriteLine($"0,0,{timestamp}");
+            }
+            
+        }
+
+        private int GetCounter(string line)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                return 1;
+            }
+
+            var counterString = line.Split(',')[0];
+            return int.Parse(counterString);
+                
+        }
+
+        private string GetFilePath()
+        {
+            return Path.Combine(_directory, _fileName);
+        }
+    }
+}
---
+++ OlympusCodeQuiz/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OlympusCodeQuiz
+{
+    internal class Program
+    {
+        const string FILE_NAME = "out.txt";
+        const string DIRECTORY = "log";
+        const int THREAD_COUNT = 10;
+        const int LINES_PER_THREAD = 10;
+
+
+        static void Main(string[] args)
+        {
+            FileLineWriter fileLineWriter = new FileLineWriter(DIRECTORY, FILE_NAME);
+            fileLineWriter.InitFile();
+
+            var writer = new FileLockLineWriter(THREAD_COUNT, LINES_PER_THREAD, fileLineWriter);
+            writer.WriteLines();
+
+            Console.WriteLine("done");
+            Console.Read();
+        }
+
+        
+    }
+}
